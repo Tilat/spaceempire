1737740028b83bdcd99027c42060b18c35c31579
Sergey <sergey@varyag.(none)>
2011-05-27 10:54:53 +0300

    [sergey] remove old version of server

--- server/core/src/main/java/com/varyag/spaceempire/core/model/user/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.varyag.spaceempire.core.model.user;
-
-/**
- * User: Sergey
- * Date: 29.05.2010 19:30:27
- */
-public class Admin extends BaseUser {
-}
--- server/core/src/main/java/com/varyag/spaceempire/core/model/user/Player.java
+++
@@ -1,9 +0,0 @@
-package com.varyag.spaceempire.core.model.user;
-
-/**
- * User: Sergey
- * Date: 29.05.2010 19:59:03
- */
-public class Player extends BaseUser{
-    
-}
